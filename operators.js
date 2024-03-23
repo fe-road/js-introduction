4d8c00eb082d0e06152f42c01fd6c7d1ec64c85b
Phellipe Perin <phellipe.perin@gmail.com>
2024-03-23 16:29:34 +0100

    operators

---
+++ operators.js
@@ -0,0 +1,33 @@
+// Addition
+let myAge = 30;
+console.log(myAge);
+
+myAge += 1;
+console.log(myAge);
+
+console.log(myAge++);
+console.log(++myAge);
+
+// Subtraction
+myAge -= 2;
+myAge--;
+--myAge;
+
+console.log(myAge);
+
+// Multiplication
+let score = 10;
+score = 5 * score;
+score *= 5;
+console.log(score);
+
+// Division
+score /= 3;
+console.log(score);
+
+// Remainder
+console.log(10 % 3);
+
+// Exponent
+console.log(Math.pow(7, 3));
+console.log(9 ** 3);
