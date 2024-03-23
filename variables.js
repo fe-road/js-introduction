cb26a021d97ca007bee9e51539cd0464244fe47c
Phellipe Perin <phellipe.perin@gmail.com>
2024-03-23 16:06:38 +0100

    first lessons

---
+++ variables.js
@@ -0,0 +1,22 @@
+// Never use!
+x;
+var y;
+
+// Namings are poor!
+let a = 1;
+const b = 2;
+
+a = 3;
+
+let c, d = 7, e = 14, f;
+c = 4;
+
+// This is good!
+const myAge = 30; // number
+const myName = 'Phellipe Perin'; // string
+const livesInEurope = true; // boolean
+
+const phoneNumber = null; // null
+const email = undefined; // undefined
+
+// symbol, bigint
