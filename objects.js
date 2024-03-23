5f8457e04adbb60530a99b33f434cd4aff6656b4
Phellipe Perin <phellipe.perin@gmail.com>
2024-03-23 21:30:58 +0100

    objects

---
+++ objects.js
@@ -0,0 +1,18 @@
+const me = {
+    name: 'Phellipe Perin',
+    age: 30,
+    friend: {
+        name: 'My Friend',
+        age: 32,
+    }
+};
+
+const anotherPerson = {};
+anotherPerson.name = 'Another name';
+
+console.log(anotherPerson.name);
+
+// console.log(me.name);
+// console.log(me['age']);
+// console.log(me.friend.name);
+// console.log(me.friend['age']);
