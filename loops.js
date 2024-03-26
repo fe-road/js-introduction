a18fdcddb03d590e69f996230241d7ad95797487
Phellipe Perin <phellipe.perin@gmail.com>
2024-03-26 10:34:17 +0100

    loops

---
+++ loops.js
@@ -0,0 +1,30 @@
+// let a = 0;
+// while (a < 5) {
+//     console.log(a);
+//     a++;
+// }
+
+
+// let b = 100;
+// do {
+//     console.log(b);
+//     b++;
+// } while (b < 10);
+
+
+for (let c = 0; c < 10; c++) {
+    console.log("c", c);
+
+    let d = 0;
+    while (d < 5) {
+        console.log("d", d);
+        d++;
+    }
+}
+
+// let livesInEurope = true;
+
+// while (livesInEurope) {
+//     const country = getRandomNewCountry();
+//     livesInEurope = isCountryInEurope(country);
+// }
